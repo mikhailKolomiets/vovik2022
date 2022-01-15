be864ad033c7546008b2a451a89f295ef0b25e10
mihail <mihail.kolomiets@greyfinch.com>
2022-01-15 15:00:11 +0200

    spring dependency downdrage fom jar manifest

--- src/main/java/com/oasis/OasisApplication.java
+++ src/main/java/com/oasis/OasisApplication.java
@@ -2,19 +2,12 @@ package com.oasis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OasisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OasisApplication.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 
 }
